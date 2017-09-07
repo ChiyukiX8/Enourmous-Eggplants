c51788fae19f7f84f345de662823bef72addea32
Dan Moore <dos-mos@comcast.net>
2017-09-06 22:23:04 -0500

    Committing first build of Enormous Eggplants

---
+++ Enormous Eggplants/CertifiedEnormousPornTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CertifiedEnormousPornTableViewCell.swift
+//  Enormous Eggplants
+//
+//  Created by Dan Moore on 9/6/17.
+//  Copyright © 2017 Hack-a-thon. All rights reserved.
+//
+
+import UIKit
+
+class CertifiedEnormousPornTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
