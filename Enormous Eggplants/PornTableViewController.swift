850454bd8345567bb2069f26f42e4b55f112b2ca
Dan Moore <Dos-Mos@comcast.net>
2017-09-09 21:24:03 -0500

    The app is now finished., and ready for the hack-a-thon.   All of the movies have been added.

--- Enormous Eggplants/PornTableViewController.swift
+++ Enormous Eggplants/PornTableViewController.swift
@@ -218,7 +218,7 @@ class PornTableViewController: UITableViewController {
         //Inrearendence Day
         var inrearendenceDayReviewArray = [PornReview]()
         let inrearendenceDayCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "Splendidly cheesy entertainment.")
-        let inrearendenceDayCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "f I were a 10-year-old boy, I'd probably think it was the coolest movie going.")
+        let inrearendenceDayCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "If I were a 10-year-old boy, I'd probably think it was the coolest movie going.")
         let inrearendenceDayUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "Good film. A bit too unrealistic for my taste but good enough.")
         let inrearendenceDayUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "It works, for all it is worth, though not much can be said of any depth or realism, it is a sci-fi flick with good old sci-fi goodness.")
         
@@ -227,7 +227,88 @@ class PornTableViewController: UITableViewController {
 
         //End Inrearendence Day
         
-        porn += [gameOfBones, forrestHump, assablanca, goodWillHumping, jurassicPork, tenInchMutantNinjaTurtles, tigerWood, assVentura, bitanic, inrearendenceDay]
+        //Pocahotass
+        var pocahotassReviewArray = [PornReview]()
+        let pocahotassCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "The film may take you by surprise with its quick ending, running only 76 minutes before the credits roll. I wanted more both times I saw it.")
+        let pocahotassCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "Daily Rub", review: "Forgettable.")
+        let pocahotassUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "For the past two years, I've had a hankering to rewatch Pocahotass to see if my childhood love for it still stands, and despite its glaring historical inaccuracies with Pocahontas's and John Smith's anal scene, boy does it truly hold up!")
+        let pocahotassUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "Fine for kids; just don't expect a history lesson.")
+        
+        pocahotassReviewArray += [pocahotassCriticReview1, pocahotassCriticReview2, pocahotassUserReview3, pocahotassUserReview4]
+        let pocahotass = Porn(title: "Pocahotass", poster: #imageLiteral(resourceName: "pocahotass-600") , rating: "57%", userScore: "64%", pornReviews: pocahotassReviewArray, reviewIcon: #imageLiteral(resourceName: "Pickle-29x29"))
+        //End Pocahotass
+        
+        //Start Sexbusters
+        var sexbustersReviewArray = [PornReview]()
+        let sexbustersCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "...an erotic yet consistently entertaining summer blockbuster...")
+        let sexbustersCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "Whoever thought of having evil's final manifestation take the form of a 100-ft. penis deserves the rational mind's eternal gratitude.")
+        let sexbustersUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "Sexbusters stands as one of the best pornos out there. It is hilarious in almost every sense of the word, even if not in the conventional way, and it never falls short with the cheesy cumshots. It can be genuinely shocking at times but that is taken away")
+        let sexbustersUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "It's almost impossible not to cum while watching it.")
+        
+        sexbustersReviewArray += [sexbustersCriticReview1, sexbustersCriticReview2, sexbustersUserReview3, sexbustersUserReview4]
+        let sexbusters = Porn(title: "Sexbusters", poster: #imageLiteral(resourceName: "porn-titles-4") , rating: "97%", userScore: "88%", pornReviews: sexbustersReviewArray, reviewIcon: #imageLiteral(resourceName: "Eggplant"))
+
+        //End Sexbusters
+        
+        //Start Womb Raider
+        var wombRaiderReviewArray = [PornReview]()
+        let wombRaiderCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "A jerky, fragmented tale that jumps from one erotic location to the next without any explanation or flow.")
+        let wombRaiderCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "Daily Rub", review: "Loud, banal, empty, frenzied, plasticized, flavorless, drab, violent in a bloodless way and sexy in a sexless way.")
+        let wombRaiderUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "User", review: "In the department of numbing ineptitude, the pacing runs a neck-and-neck race with the dialogue.")
+        let wombRaiderUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "User", review: "Some great anal sequences, but not much else.")
+        
+        wombRaiderReviewArray += [wombRaiderCriticReview1, wombRaiderCriticReview2, wombRaiderUserReview3, wombRaiderUserReview4]
+        let wombRaider = Porn(title: "Womb Raider", poster: #imageLiteral(resourceName: "porn-titles-6") , rating: "20%", userScore: "47%", pornReviews: wombRaiderReviewArray, reviewIcon: #imageLiteral(resourceName: "Pickle-29x29"))
+
+        //End Womb Raider
+        
+        //John and Kate Fuck 8
+        var johnAndKateReviewArray = [PornReview]()
+        let johnAndKateCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "Top quality gang bang action.")
+        let johnAndKateCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "LTLC has really outdone themselves.")
+        let johnAndKateUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "John and Kate are the real stars of the show")
+        let johnAndKateUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "Nothing else like it.")
+        
+        johnAndKateReviewArray += [johnAndKateCriticReview1, johnAndKateCriticReview2, johnAndKateUserReview3, johnAndKateUserReview4]
+        let johnAndKateFuck8 = Porn(title: "John and Kate Fuck 8", poster: #imageLiteral(resourceName: "porn-titles-10") , rating: "70%", userScore: "66%", pornReviews: johnAndKateReviewArray, reviewIcon: #imageLiteral(resourceName: "Eggplant"))
+        //End John and Kate Fuck 8
+        
+        //XXXorcist
+        var xxxorcistReviewArray = [PornReview]()
+        let xxxorcistCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "The XXXorcist, with its excellent cast, mounting intensity, and ingeniously constructed surprises, is still a commanding achievement.")
+        let xxxorcistCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "The XXXorcist, like most memorable porn movies, gains its power from the way it mixes opposites: new-style realism and sexual radicalism, old-style oral and anal.")
+        let xxxorcistUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "A work of porn in its own league.")
+        let xxxorcistUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "User", review: "Takes its audience for a ride, spewing it out the other end.")
+        
+        xxxorcistReviewArray += [xxxorcistCriticReview1, xxxorcistCriticReview2, xxxorcistUserReview3, xxxorcistUserReview4]
+        let xxxorcist = Porn(title: "The XXXorcist", poster: #imageLiteral(resourceName: "porn-titles-13") , rating: "86%", userScore: "87%", pornReviews: xxxorcistReviewArray, reviewIcon: #imageLiteral(resourceName: "Eggplant"))
+        //End XXXorcist
+        
+        //Start Honey I Blew
+        var blewEverybodyReviewArray = [PornReview]()
+        let blewEverybodyCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "Sterling entertainment...")
+        let blewEverybodyCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "As sweet, straightforward and funny as its title.")
+        let blewEverybodyUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "User", review: "Two-dimensional boredom.")
+        let blewEverybodyUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "User", review: "One of the worst live-action flicks from the '80s.")
+        
+        blewEverybodyReviewArray += [blewEverybodyCriticReview1, blewEverybodyCriticReview2, blewEverybodyUserReview3, blewEverybodyUserReview4]
+        let blewEverybody = Porn(title: "Honey, I Blew Everybody", poster: #imageLiteral(resourceName: "porn-titles-14") , rating: "75%", userScore: "53%", pornReviews: blewEverybodyReviewArray, reviewIcon: #imageLiteral(resourceName: "Eggplant"))
+
+        //End Honey I Blew
+        
+        //Start Edward Penishands
+        var edwardPenishandsReviewArray = [PornReview]()
+        let edwardPenishandsCriticReview1 = PornReview(reviewerName: "Ron Jeremy", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Footlong Magazine", review: "A delightful and delicate comic porno.")
+        let edwardPenishandsCriticReview2 = PornReview(reviewerName: "John Wiener", reviewerIcon: #imageLiteral(resourceName: "Eggplant"), reviewerMagazineOrTissueRating: "Daily Rub", review: "If you can only watch one porno about a man with penises for hands, watch Edward Penishands.")
+        let edwardPenishandsUserReview3 = PornReview(reviewerName: "Joe Humpfree", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "User", review: "He didn't really cum all that much for having penises for hands..")
+        let edwardPenishandsUserReview4 = PornReview(reviewerName: "Tommy Salami", reviewerIcon: #imageLiteral(resourceName: "Pickle-29x29"), reviewerMagazineOrTissueRating: "User", review: "Wasted potential.")
+        
+        edwardPenishandsReviewArray += [edwardPenishandsCriticReview1, edwardPenishandsCriticReview2, edwardPenishandsUserReview3, edwardPenishandsUserReview4]
+        let edwardPenishands = Porn(title: "Edward Penishands", poster: #imageLiteral(resourceName: "porn-titles-21") , rating: "89%", userScore: "91%", pornReviews: edwardPenishandsReviewArray, reviewIcon: #imageLiteral(resourceName: "Eggplant"))
+
+        //End Edward Penishands
+        
+        porn += [gameOfBones, forrestHump, assablanca, goodWillHumping, jurassicPork, tenInchMutantNinjaTurtles, tigerWood, assVentura, bitanic, inrearendenceDay, pocahotass, sexbusters, wombRaider, johnAndKateFuck8, xxxorcist, blewEverybody, edwardPenishands]
         
         
     }
